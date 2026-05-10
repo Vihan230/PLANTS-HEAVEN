4d48016b947ee498c624e018e1514b89c7623d5b
Vihan230 <vihansontyalar@gmail.com>
2026-05-10 17:15:09 +0530

    Completed files

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function sendOrder() {
-  fetch("https://api.telegram.org/bot8677445268:AAGasyBNAWKMowF8ic835c5bSu0ZOh6WwGg/sendMessage", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      chat_id: "5284242303",
-      text: "🌱 New Order Received on your website!"
-    })
+  const botToken = "8677445268:AAGasyBNAWKMowF8ic835c5bSu0ZOh6WwGg";
+  const chatIds = [
+    "5284242303", 
+    "8196306889"
+  ];
+
+  const message = "🌱 New Order Received on your website!";
+
+  chatIds.forEach(chatId => {
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: message
+      })
+    });
   });
 
-  alert("Order placed!");
+  alert("Order placed! Notifications sent.");
 }
